0262d9d53c04a25a260b396483828e24710bfbac
Jakub Lach <jakub.lach@gmail.com>
2024-09-28 13:00:12 +0200

    added support for video finger tracking

--- video_hand_tracking.py
+++ video_hand_tracking.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2 as cv
 import mediapipe as mp
 from mediapipe.tasks import python
@@ -10,11 +9,12 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 base_options = python.BaseOptions(model_asset_path=r'C:\Users\jakub\Downloads\hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options=base_options,
-                                       num_hands=2,
+                                       num_hands=1,
                                        running_mode=VisionRunningMode.VIDEO)
 detector = vision.HandLandmarker.create_from_options(options)
 
 cap = cv.VideoCapture(r'C:\Users\jakub\Downloads\signal-2024-09-27-192348.mp4')
+
 frame_rate = cap.get(cv.CAP_PROP_FPS)
 time_per_frame_ms = (1 / frame_rate)*1000
 frame_number = 0
@@ -27,7 +27,7 @@ while cap.isOpened():
         break
 
     timestamp = frame_number * time_per_frame_ms
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame, cv.COLOR_BGR2RGB))
     detection_result = detector.detect_for_video(mp_image, int(timestamp))
     annotated_image = handle_image.draw_landmarks_on_image(mp_image.numpy_view(), detection_result)
     cv.imshow('img', cv.cvtColor(annotated_image, cv.COLOR_RGB2BGR))
